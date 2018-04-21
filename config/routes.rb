dbeb97eec64db7a793ae1edd0717a394abb5ad6d
ChasobaAlpacanist <chasobakun@gmail.com>
2018-04-21 12:01:10 +0900

    forgot to add ,

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'hello#show'
-  get '/static_pages/home' to: 'static_pages#home'
-  get '/static_pages/help' to: 'static_pages#help'
+  get '/static_pages/home', to: 'static_pages#home'
+  get '/static_pages/help', to: 'static_pages#help'
 end
