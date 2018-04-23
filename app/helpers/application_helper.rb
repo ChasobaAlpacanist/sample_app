05c3401dfe0d3f942117a18925a1cbf426d8a96f
ChasobaAlpacanist <chasobakun@gmail.com>
2018-04-23 18:47:44 +0900

    add helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+#引数以降の = ''はデフォルト引数を表す。
+  def full_title(page_title = '')
+    base_title = 'Ruby on Rails Tutorial Sample App'
+    if page_title.empty?
+      base_title
+    else
+      page_title + ' | ' + base_title
+    end
+  end
 end
