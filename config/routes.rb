e480f4fbaa8bdd04b1865ca0680004334df6b831
ChasobaAlpacanist <chasobakun@gmail.com>
2018-05-02 18:26:38 +0900

    add partial and contact.html

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/static_pages/home', to: 'static_pages#home'
-  get '/static_pages/help', to: 'static_pages#help'
-  get '/static_pages/about', to: 'static_pages#about'
+  get '/static_pages/home', to: 'static_pages#home', as: 'home_path'
+  get '/static_pages/help', to: 'static_pages#help', as: 'help_path'
+  get '/static_pages/about', to: 'static_pages#about', as: 'about_path'
   root 'hello#show'
 end
