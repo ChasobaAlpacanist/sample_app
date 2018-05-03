bafc15aa2ff939a293edc5be838da10304cb5a5c
ChasobaAlpacanist <chasobakun@gmail.com>
2018-05-03 11:45:11 +0900

    add test and fixed contents

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/static_pages/home', to: 'static_pages#home', as: 'home_path'
-  get '/static_pages/help', to: 'static_pages#help', as: 'help_path'
-  get '/static_pages/about', to: 'static_pages#about', as: 'about_path'
-  root 'hello#show'
+  get 'help', to:'static_pages#help'
+  get 'about', to:'static_pages#about'
+  get 'contact', to:'static_pages#contact'
+  root 'static_pages#home'
 end
